e1d1c81385bdca20a523523634c4bc8de12a1627
TL <tanyasltatiana@gmail.com>
2018-09-12 12:45:28 -0500

    third commit

--- hello.c
+++ hello.c
@@ -16,3 +16,13 @@ Feature 1
 
 
 
+to resolve conflict, open file and just change what need, save, git sees it. commit it afterwards.
+
+process:
+$ git status      checks what's not on repo
+$ git add hello.c
+$ git commit -m "second commit"
+$ git push -u origin master
+
+
+
